7843637d9a090c3483533b3aa769b3926387a4e6
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2020-07-07 16:50:22 +0100

    Managed to get summary output displayed

--- SEM/app.R
+++ SEM/app.R
@@ -8,42 +8,96 @@
 #
 
 library(shiny)
+library(shinyAce)
+library(piecewiseSEM)
+library(nlme)
 
-# Define UI for application that draws a histogram
+rawd<-read.csv("www/FINAL_SWEDEN.csv", header=TRUE)
+
+init <- "
+                      big_sweden_mod<-psem(
+                      lme(humans  ~ time   + habitat1,                              random=~1|name, rawd),
+                      lme(seed    ~ humans + habitat1,                              random=~1|name, rawd),
+                      lme(wolf    ~ humans + seed     + habitat1,                   random=~1|name,rawd),
+                      lme(lynx    ~ humans + seed     + habitat1,                   random=~1|name,rawd),
+                      lme(red_fox ~ humans + seed     + wolf     + lynx + habitat1, random=~1|name, rawd)
+                      )
+                      
+                      
+"
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+    h1("Shiny Ace Evaluate Code"),
+    fluidRow(
+        column(
+            6,
+            h2("Source Code"),
+            aceEditor("code", mode = "r", height = "200px", value = init),
+            actionButton("eval", "Evaluate")
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        column(
+            6,
+            h2("Output"),
+            verbatimTextOutput("outputtext")
         )
     )
 )
 
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    myvals <- reactive({
+        input$eval
+        x <- isolate({
+            mystuff <- paste(input$code,
+                             "summary(big_sweden_mod)")
+        })
     })
+    
+    output$outputtext <- renderPrint({
+        # input$eval
+        # x <- isolate({
+        #   input$code
+        # })
+        #parse(text = x) # Returns everything in the double quotes above
+        #eval(parse(text = x))
+        #eval(parse(text = isolate(input$code)))
+        eval(parse(text = myvals()))
+        #return(myres)
+    })
+    
 }
+
+
+
+# server <- function(input, output) {
+#     
+#     big_sweden_mod <- reactive({
+#         #sem_results <- psem(input$semeditor)
+#         sem_results <- input$editor
+#         # sem_results<-psem(
+#         #     lme(humans~time+habitat1,random=~1|name,rawd),
+#         #     lme(seed~humans+habitat1,random=~1|name, rawd),
+#         #     lme(wolf~humans+seed+habitat1,random=~1|name,rawd),
+#         #     lme(lynx~humans+seed+habitat1,random=~1|name,rawd),
+#         #     lme(red_fox~humans+seed+wolf+lynx+habitat1, random=~1|name, rawd))
+#         
+#         
+#         return(sem_results)
+#     })
+#     
+#     # output$semplot <- renderPlot({
+#     #     plot(big_sweden_mod(), show="std",node_attrs = list(
+#     #         shape = "circle", color = "black",
+#     #         fillcolor = "orange"))
+#     # })
+#     
+#     output$semsummary <- renderPrint({
+#         class(big_sweden_mod())
+#     })
+# 
+# }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
